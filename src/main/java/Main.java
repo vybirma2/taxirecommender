bcb681dc1f898a0fe04fea0e3289fa5e83857e98
vybirma2 <martinvybi@seznam.cz>
2020-04-12 18:17:35 +0200

    data files generated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
         try {
             taxiRecommenderDomainGenerator = new TaxiRecommenderDomainGenerator(
                     "prague_full.fst",
-                    "prague_charging_stations_full.json", new GridEnvironment());
+                    "prague_charging_stations_full.json", new KMeansEnvironment());
         } catch (Exception e) {
             e.printStackTrace();
         }
